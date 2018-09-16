db0ea1d2b0bc4954d3969633fc320b549a79a704
Codrut Alexandru <panea_codrut@yahoo.com>
2018-09-16 13:49:45 +0300

    make car-list page look good on mobile

--- src/app/pages/car-list/car-list.page.ts
+++ src/app/pages/car-list/car-list.page.ts
@@ -10,11 +10,11 @@ export class CarListPage implements OnInit {
 
     cars: CarModel[] = [];
     carModel1: CarModel = new CarModel(1, 'Skoda Rapid', 4, 'Manual', 'B-55-CAR', true,
-        9, './src/assets/img/skoda-rapid.png', 'Benzina', 4);
+        9, 'assets/img/skoda-rapid.png', 'Benzina', 4);
     carModel2: CarModel = new CarModel(1, 'Skoda Citigo', 4, 'Automatic', 'B-15-CAR', true,
-        9, './src/assets/img/skoda-citigo.png', 'Benzina', 4);
+        9, 'assets/img/skoda-citigo.png', 'Benzina', 4);
     carModel3: CarModel = new CarModel(1, 'VW Transporter', 4, 'Automatic', 'B-23-CAR', true,
-        9, './src/assets/img/vw-transporter-shuttler.png', 'Benzina', 8);
+        9, 'assets/img/vw-transporter-shuttler.png', 'Benzina', 8);
 
     constructor() {
     }
